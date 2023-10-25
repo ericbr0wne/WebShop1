6cfabcdfcdc01fe51ada64740f1f667ad9b2d21d
BennyAhlin <Benny.Ahlin@hotmail.com>
2023-10-25 13:40:08 +0200

    How to add new user

---
+++ NewUser.cs
@@ -0,0 +1,52 @@
+﻿
+using System.Threading.Channels;
+
+namespace WebShop1;
+//create()
+//string[] users = File.ReadAllLines("users.csv");
+//Setup() 
+
+
+public class NewUser
+{
+
+    public static void Create()
+    {
+        string username = Console.ReadLine() ?? string.Empty;
+
+        string[] users = File.ReadAllLines("users.csv");
+
+        bool exists = false;
+        foreach (string user in users)
+        {
+            if (user.Split(",")[0].Equals(username))
+            {
+                Console.WriteLine("User already exists");
+                exists = true;
+            }
+
+            if (!exists)
+            {
+
+                string password = Console.ReadLine() ?? string.Empty;
+                string newUser = $"{username}, {password}{Environment.NewLine}";
+                File.AppendAllText("users.csv", newUser);
+            }
+        }
+    }
+
+    public static void Setup()
+    {
+        if (!File.Exists("users.csv"))
+        {
+            File.Create("users.csv").Close();
+        }
+    }
+
+
+
+
+
+
+
+}
