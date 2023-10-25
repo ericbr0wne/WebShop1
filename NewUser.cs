b5237955c2b3db954de7175dce613eb47a3f13c5
TheAceOfJ <arvid.jannhov@hotmail.com>
2023-10-25 14:11:12 +0200

    name fixes

--- NewUser.cs
+++ NewUser.cs
@@ -44,7 +44,30 @@ public class NewUser
     }
 
 
+    public static void CreateFile()
+    {
+        string username = Console.ReadLine() ?? string.Empty;
 
+        string[] users = File.ReadAllLines("users.csv");
+
+        bool exists = false;
+        foreach (string user in users)
+        {
+            if (user.Split(",")[0].Equals(username))
+            {
+                Console.WriteLine("User already exists");
+                exists = true;
+            }
+
+            if (!exists)
+            {
+
+                string password = Console.ReadLine() ?? string.Empty;
+                string newUser = $"{username}, {password}{Environment.NewLine}";
+                File.AppendAllText("users.csv", newUser);
+            }
+        }
+    }
 
 
 
